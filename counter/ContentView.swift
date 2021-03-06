38baf7ad8e3bb7d7fc60c85514f38c8a8726c7cd
josephrt <josephrti@gmail.com>
2021-03-06 10:00:18 +0300

    Initial Commit

---
+++ counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  counter
+//
+//  Created by Егор Уваров on 06.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
